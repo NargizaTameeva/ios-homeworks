abf29dd8ac922410976468154d11383defd6ad00
NargizaTameeva <nargizochilova1992@gmail.com>
2023-06-05 13:27:16 +0300

    Added  collection with  vertical scroll to the PhotosTableViewCell

--- Navigation/Profile/PhotosTableViewCell.swift
+++ Navigation/Profile/PhotosTableViewCell.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-class PhotosTableViewCell: UITableViewCell {
-    
+class PhotosTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
     static let id = "PhotosTableViewCell"
-    
+
     lazy var photosLabel: UILabel = {
            let label = UILabel()
            label.text = "Photos"
@@ -19,7 +19,7 @@ class PhotosTableViewCell: UITableViewCell {
            label.translatesAutoresizingMaskIntoConstraints = false
            return label
        }()
-    
+
     lazy var photosButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "arrow.right"), for: .normal)
@@ -27,106 +27,79 @@ class PhotosTableViewCell: UITableViewCell {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-    
-    lazy var ImageView1: UIImageView = {
-           let imageView = UIImageView()
-           imageView.image = UIImage(named: "photo0")
-           imageView.translatesAutoresizingMaskIntoConstraints = false
-           imageView.clipsToBounds = true
-           imageView.layer.cornerRadius = 6
-           return imageView
-       }()
-    
-    lazy var ImageView2: UIImageView = {
-           let imageView = UIImageView()
-           imageView.image = UIImage(named: "photo1")
-           imageView.translatesAutoresizingMaskIntoConstraints = false
-           imageView.clipsToBounds = true
-           imageView.layer.cornerRadius = 6
-           return imageView
-       }()
-    
-    lazy var ImageView3: UIImageView = {
-           let imageView = UIImageView()
-           imageView.image = UIImage(named: "photo2")
-           imageView.translatesAutoresizingMaskIntoConstraints = false
-           imageView.clipsToBounds = true
-           imageView.layer.cornerRadius = 6
-           return imageView
-       }()
-    
-    lazy var ImageView4: UIImageView = {
-           let imageView = UIImageView()
-           imageView.image = UIImage(named: "photo3")
-           imageView.translatesAutoresizingMaskIntoConstraints = false
-           imageView.clipsToBounds = true
-           imageView.layer.cornerRadius = 6
-           return imageView
-       }()
-    
-   
-    
+
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 8
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.showsVerticalScrollIndicator = false
+        cv.backgroundColor = .clear
+        cv.delegate = self
+        cv.dataSource = self
+        cv.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: PhotosCollectionViewCell.id)
+        return cv
+    }()
+
+  
+
     weak var parentViewController: UIViewController?
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
-    
+      
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-   
+
     private func setupUI() {
         addSubview(photosLabel)
         addSubview(photosButton)
-        addSubview(ImageView1)
-        addSubview(ImageView2)
-        addSubview(ImageView3)
-        addSubview(ImageView4)
+        addSubview(collectionView)
 
-        let screenWidth = UIScreen.main.bounds.width
+      let screenWidth = UIScreen.main.bounds.width
         let offset = CGFloat(12)
 
         NSLayoutConstraint.activate([
-                photosLabel.topAnchor.constraint(equalTo: topAnchor, constant: offset),
-                photosLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: offset),
-                
-                photosButton.centerYAnchor.constraint(equalTo: photosLabel.centerYAnchor),
-                photosButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -offset),
-                photosButton.widthAnchor.constraint(equalToConstant: 24), // Задаем ширину кнопки
-                photosButton.heightAnchor.constraint(equalToConstant: 24), // Задаем высоту кнопки
-                
-                ImageView1.topAnchor.constraint(equalTo: photosLabel.bottomAnchor, constant: offset),
-                ImageView1.leadingAnchor.constraint(equalTo: leadingAnchor, constant: offset),
-                ImageView1.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -offset),
-                ImageView1.widthAnchor.constraint(equalToConstant: (screenWidth - offset * 4) / 4),
-                ImageView1.heightAnchor.constraint(equalToConstant: (screenWidth - offset * 4) / 4),
+            photosLabel.topAnchor.constraint(equalTo:topAnchor, constant: offset),
+            photosLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: offset),
 
-                ImageView2.topAnchor.constraint(equalTo: photosLabel.bottomAnchor, constant: offset),
-                ImageView2.leadingAnchor.constraint(equalTo:ImageView1.trailingAnchor, constant: 8),
-                ImageView2.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -offset),
-                ImageView2.widthAnchor.constraint(equalTo: ImageView1.widthAnchor),
-                ImageView2.heightAnchor.constraint(equalTo: ImageView1.heightAnchor),
+            photosButton.centerYAnchor.constraint(equalTo: photosLabel.centerYAnchor),
+            photosButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -offset),
+            photosButton.widthAnchor.constraint(equalToConstant: 24),
+            photosButton.heightAnchor.constraint(equalToConstant: 24),
 
-                ImageView3.topAnchor.constraint(equalTo: photosLabel.bottomAnchor, constant: offset),
-                ImageView3.leadingAnchor.constraint(equalTo: ImageView2.trailingAnchor, constant: 8),
-                ImageView3.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -offset),
-                ImageView3.widthAnchor.constraint(equalTo: ImageView1.widthAnchor),
-                ImageView3.heightAnchor.constraint(equalTo: ImageView1.heightAnchor),
+            collectionView.topAnchor.constraint(equalTo: photosLabel.bottomAnchor, constant: offset),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: offset),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -offset),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -offset),
+            collectionView.heightAnchor.constraint(equalToConstant: (screenWidth - offset * 4) / 4 )
+           
+        ])
+    }
 
-                ImageView4.topAnchor.constraint(equalTo: photosLabel.bottomAnchor, constant: offset),
-                ImageView4.leadingAnchor.constraint(equalTo: ImageView3.trailingAnchor, constant: 8),
-                ImageView4.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -offset),
-                ImageView4.widthAnchor.constraint(equalTo: ImageView1.widthAnchor),
-                ImageView4.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -offset),
-                ImageView4.heightAnchor.constraint(equalTo: ImageView1.heightAnchor),
-                
-            ])
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotosCollectionViewCell.id, for: indexPath) as! PhotosCollectionViewCell
+        let photo = photos[indexPath.row]
+        cell.setup(with: photo)
+        return cell
+    }
+
+   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+       let screenWidth = UIScreen.main.bounds.width
+       let offset = CGFloat(12)
+       let cellWidth = (screenWidth - offset * 4) / 4
+       return CGSize(width: cellWidth, height: cellWidth)
     }
 
 }
-
